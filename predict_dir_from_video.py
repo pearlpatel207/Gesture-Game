50ddd6334c785656ecc01ad70eba6b2e81b43d3b
Pearl Patel <48798821+pearlpatel207@users.noreply.github.com>
2020-03-09 14:49:53 +0530

    Update predict_dir_from_video.py

--- predict_dir_from_video.py
+++ predict_dir_from_video.py
@@ -3,6 +3,8 @@ import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
 
+labels = ['right', 'left', 'up', 'down']
+
 np.set_printoptions(suppress=True)
 model = tensorflow.keras.models.load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -12,13 +14,14 @@ video_capture = cv2.VideoCapture(1)
 process_this_frame = True
 
 while True:
+    
     ret, frame = video_capture.read()
+    size = (224, 224)
+    small_frame = cv2.resize(frame, size)
 
     if process_this_frame:
-        size = (224, 224, 3)
-        #image = ImageOps.fit(image, size, Image.ANTIALIAS)
-        image = frame.resize(size)
-        image_array = np.asarray(frame)
+        
+        image_array = np.asarray(small_frame)
         normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
         data[0] = normalized_image_array
 
@@ -27,7 +30,7 @@ while True:
     process_this_frame = not process_this_frame
 
     font = cv2.FONT_HERSHEY_DUPLEX
-    txt = str(prediction.argmax())
+    txt = labels[prediction.argmax()]
     cv2.putText(frame, txt, (116,90), font, 2.0, (255, 255, 255), 1)
     cv2.imshow('Video', frame)
 
